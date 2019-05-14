b073c75b8ce7ac82a1c29f93f2e8ff40a4a82949
cetecom <cetecom@LCV11010>
2019-05-14 14:50:16 -0400

    Formulario basico sin Bootstrap

--- forms/formcampeonato.php
+++ forms/formcampeonato.php
@@ -1,8 +1,39 @@
-<?php
+<?php 
+    include('../lib/constantes.php')
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Formulario Campeonato</title>
+    </head>
+    <body>
+        
+        <div id="contenedor">
+            <div id="titulo"></div>
+            <div id="menu"></div>
+                    <div id="contenido">
+                        <form action="../lib/db.php" method="get">
+                            Id campeonato: <input id="idcampeonato" name="id" type="text">
+                            <br>Codigo: <input id="codigo" name="codigo" type="text">
+                            <br>Nombre: <input id="nombre" name="nombre" type="text">
+                            <br>Fecha de inicio:<input id="fechaini" name="fechaini" type="date">
+                            <br>Fecha de termino:<input id="fechatermi" name="fechatermi" type="date">
+                            <br>Cantidad de partidos: <input id="cantidad" name="cantidad" type="text">
+                            <input type="submit" value="Enviar"  >                     
+                        </form>
+                    </div>
+        </div>
+        
+    </body>
+    
 
+    
+    
+</html>
