36bc028a117431da091092623b76836bbacebb3d
cetecom <cetecom@LCV11000>
2019-05-14 13:59:58 -0400

    Base de programacion

---
+++ lib/constantes.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+session_start();
+define("URLBASE",'http://localhost:8081/colaboracion/');
+define("DIRBASE",'C:/xampp/htdocs/colaboracion');
+/*
+ * Parametros DB
+ * **/
+define("DBNAME",'equipos');
+define("DBUSR",'duoc');
+define("DBPWD",'');
+define("DBHOST",'localhost');
