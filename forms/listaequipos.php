76e27fb7307fd80a382a8f02aa5d8af0c270c2f6
cetecom <cetecom@LCV11017>
2019-05-14 14:59:36 -0400

    Lo que quedo

--- forms/listaequipos.php
+++ forms/listaequipos.php
@@ -4,20 +4,7 @@ include ("../lib/db.php");
 include("../lib/constantes.php");
 echo "<table style='border: solid 1px black;'>";
 echo "<tr><th>Codigo</th><th>Nombre</th></tr>";
-class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
-    }
-    function current() {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
-    function beginChildren() {
-        echo "<tr>";
-    }
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
-}
+
 $db= new DBConnect();
         $dblink=$db->conexion();
         
