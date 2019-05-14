c1613dc9040da64d620c4e14fe15f1b19ed8bfc7
cetecom <cetecom@LCV11017>
2019-05-14 14:40:15 -0400

    Listar Equipos realizado

--- forms/listaequipos.php
+++ forms/listaequipos.php
@@ -1,8 +1,35 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php 
+include ("../clases/equipos.php");
+include ("../lib/db.php");
+include("../lib/constantes.php");
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Codigo</th><th>Nombre</th></tr>";
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+    function beginChildren() {
+        echo "<tr>";
+    }
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+$db= new DBConnect();
+        $dblink=$db->conexion();
+        
+        if (!isset($dblink)){
+            return false;
+        }
+        $stmt = $dblink->prepare("SELECT codigo, nombre FROM equipo");
+    $stmt->execute();
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+echo "</table>";
+?> 
