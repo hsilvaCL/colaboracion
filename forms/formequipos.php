b42b88d5144bcaa3572d94f397b104a39fb79832
cetecom <cetecom@LCV11013>
2019-05-14 15:01:50 -0400

    intento fuli number one

--- forms/formequipos.php
+++ forms/formequipos.php
@@ -1,8 +1,28 @@
 <?php
+//LA PARTE DE FULI
+
+
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open 
+ * 
+ * 
  */
+?>
+ 
+ <html>
+<body>
+
+<form action="equipo.php" method="post">
+Codigo: <input type="text" name="codigo"><br>
+Nombre: <input type="text" name="nombre"><br>
+CantPartidos: <input type="text" name="cantpartidos">
+<input type="submit">
+</form>
+
+</body>
+</html>
+
 
